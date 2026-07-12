696ce87337523eb4f499655d17270b6434c62b5d
Riteshsoni143 <riteshsoni35468@gmail.com>
2026-07-12 16:48:45 +0530

    change

--- src/app/service/[name]/page.jsx
+++ src/app/service/[name]/page.jsx
@@ -1,41 +1,17 @@
-// Dynamic Router
-"use client";
-import { useParams } from 'next/navigation';
-import React, { useEffect, useState, useTransition } from 'react'
-import NotFound from '@/app/not-found';
-import ServicesData from '../../api/service.json'
-import Loading from '@/app/loading';
-import ServiceDetails from '../../components/common/serviceDetails'
+import ServicesData from "../../api/service.json";
+import ServiceDetails from "../../components/common/serviceDetails";
+import { notFound } from "next/navigation";
 
-function Page() {
-    const params = useParams();
-    // console.log(params);
+export default async function Page({ params }) {
+  const { name } = await params;
 
-    const [isPending, startTransition] = useTransition();
-    const [service, setService] = useState();
+  const service = ServicesData.find(
+    (item) => item.name === decodeURIComponent(name)
+  );
 
-    useEffect(() => {
-        startTransition(() => {
-            const serviceData = ServicesData.find(
-                (item) => item.name === decodeURIComponent(params.name)
-            );
+  if (!service) {
+    notFound();
+  }
 
-            setService(serviceData);
-            console.log(serviceData);
-        })
-    }, [params.name])
-
-    if(isPending) {
-        return <Loading/>
-    }
-
-    if(!service) {
-        return <NotFound/>
-    }
-
-    return (
-        <ServiceDetails service = {service} />
-    )
+  return <ServiceDetails service={service} />;
 }
-
-export default Page;
