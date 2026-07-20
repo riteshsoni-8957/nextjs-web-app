c096e3139941bf1792928f7911ac46a10ff28dc6
Riteshsoni143 <riteshsoni35468@gmail.com>
2026-07-20 21:58:35 +0530

    changes

--- src/app/components/common/serviceDetails.jsx
+++ src/app/components/common/serviceDetails.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+"use client";
 
-function serviceDetails({service}) {
-    return (
-        <div className="max-w-5xl mx-auto p-10">
-            <img
-                src={service.image}
-                alt={service.name}
-                className="w-full h-96 object-cover rounded-lg"
-            />
+import Loading from "@/app/loading";
+import Image from "next/image";
 
-            <h1 className="text-4xl font-bold mt-6">
-                {service.name}
-            </h1>
+function ServiceDetails({ service }) {
+  if (!service) {
+    return <Loading />;
+  }
 
-            <p className="mt-4 text-gray-600">
-                {service.description}
-            </p>
-        </div>
-    );
+  return (
+    <div className="max-w-5xl mx-auto p-10">
+        <Image
+        src={service.image}
+        alt={service.name}
+        width={900}
+        height={400}
+        className="w-full h-96 object-cover rounded-lg"
+        />
+
+        <h1 className="text-4xl font-bold mt-6">
+            {service.name}
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+            {service.description}
+        </p>
+    </div>
+  );
 }
 
-export default serviceDetails
+export default ServiceDetails;
